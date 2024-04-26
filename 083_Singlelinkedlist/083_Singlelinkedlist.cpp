6e435802df2d3e1fad4331cee5dd95d789002796
feraistanti <fistanti551@gmail.com>
2024-04-26 15:20:22 +0700

    Membuat codingan searchNode

--- 083_Singlelinkedlist/083_Singlelinkedlist.cpp
+++ 083_Singlelinkedlist/083_Singlelinkedlist.cpp
@@ -31,3 +31,43 @@ void addNode() {
         return;
      
     }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
